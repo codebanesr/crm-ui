f0e703fa89a16a81e91ccf286edd2c57c15a92fc
shanur rahman <shanur.rahman@gamechangesns.com>
2020-05-26 18:32:53 +0400

    feat: [SK-0000] send for demo

--- src/app/pages/welcome/ticket-create/ticket-create.component.ts
+++ src/app/pages/welcome/ticket-create/ticket-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { TicketsService } from 'src/app/tickets.service';
 import { ActivatedRoute } from '@angular/router';
