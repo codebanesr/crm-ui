ef261a77dcab3d5ec0e9db0557a4fd3506f739bf
shanur rahman <shanur.rahman@gamechangesns.com>
2020-09-26 17:19:52 +0530

    saving changes that work

--- src/app/pages/welcome/lead-create/lead-create.component.ts
+++ src/app/pages/welcome/lead-create/lead-create.component.ts
@@ -1,204 +1,112 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { LeadsService } from 'src/app/leads.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { ActivatedRoute } from '@angular/router';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { ILeadColumn } from '../leads/lead.interface';
 import { CampaignService } from '../campaign.service';
-import { NzFormatEmitEvent } from 'ng-zorro-antd/tree';
-import { NzContextMenuService, NzDropdownMenuComponent } from 'ng-zorro-antd/dropdown';
-
+import { ILead } from 'src/interfaces/leads.interface';
+import { ClassValidationError } from 'src/global.interfaces';
 
 @Component({
   selector: 'app-lead-create',
   templateUrl: './lead-create.component.html',
-  styleUrls: ['./lead-create.component.scss']
+  styleUrls: ['./lead-create.component.scss'],
 })
 export class LeadCreateComponent implements OnInit {
-
-
-  validateForm!: FormGroup;
-  leadOptions: string[] = [];
-  ticketId: string;
-
   constructor(
-    private fb: FormBuilder,
     private leadsService: LeadsService,
-    private msg: NzMessageService,
-    private activatedRouter: ActivatedRoute,
     private campaignService: CampaignService,
-    private nzContextMenuService: NzContextMenuService
+    private msgService: NzMessageService,
+    private activatedRouter: ActivatedRoute
   ) {}
 
 
-  demoDispositionNodes: any[] = []
+
+  selectedCampaign: string;
+  selectedLead: ILead;
+  typeDict: any;
+  objectkeys = Object.keys;
+  dateMode: string = 'time';
+  loadingCampaignList = false;
+  campaignList: any[] = [];
+  callDispositions;
   ngOnInit(): void {
-    this.initForm();
-    this.mapInternalToReadableFields();
     this.subscribeToQueryParamChange();
-    this.initDispositionCore('core');
-    this.subscribeToLeadIdChange();
+    this.getLeadMappings();
+    this.populateCampaignDropdown("");
   }
 
-  nzEvent(event: NzFormatEmitEvent): void {
-    if (event.node.isLeaf) {
-      console.log("set this to formControl", event.node.origin.title);
-      this.validateForm.patchValue({ leadStatus: event.node.origin.title });
-    }
-    event.node.isExpanded = !event.node.isExpanded;
+  populateCampaignDropdown(filter) {
+    this.loadingCampaignList = true;
+    this.campaignService.getCampaigns(1, 20, filter, "", 'asc').subscribe((result: any) => {
+      this.loadingCampaignList = false;
+      this.campaignList = result.data;
+    }, error => {
+        this.loadingCampaignList = false;
+        console.log(error);
+    })
   }
 
-  initDispositionCore(campaignId: string) {
-    this.campaignService.getDisposition(campaignId).subscribe((data: any) => {
-      console.log(data);
-      this.demoDispositionNodes = data.options;
+  getDispositionForCampaign(campaignName: string) {
+    this.campaignService.getDisposition(campaignName).subscribe((data: any)=>{
+      this.callDispositions = data.options;
     }, error => {
         console.log(error);
     })
   }
 
+  leadStatusOptions: string[];
+  selectedLeadStatus: string;
+  async getLeadMappings() {
+    const { typeDict } = await this.leadsService.getLeadMappings(this.selectedCampaign);
+    this.typeDict = typeDict;
+    this.leadStatusOptions = this.typeDict.leadStatus.options;
+  }
+
+  disabledKeys = ['externalId', 'createdAt', 'updatedAt', '_id']
+  isDisabled(leadKey: string) {
+    if (this.disabledKeys.includes(leadKey)) {
+      return true;
+    }
+    return false;
+  }
+
+  handleLeadSubmission(lead: ILead) {
+    this.leadsService.updateLead(lead.externalId, lead).subscribe(data => {
+      this.msgService.success("Successfully updated lead");
+    }, ({error}: {error: ClassValidationError}) => {
+        this.msgService.error(error.message[0]);
+    });
+  }
+
+  handleDispositionTreeEvent(event) {
+    if (event.node.isLeaf) {
+      console.log("set this to formControl", event.node.origin.title);
+      // this.validateForm.patchValue({ leadStatus: event.node.origin.title });
+    }
+    event.node.isExpanded = !event.node.isExpanded;
+  }
+
+  handleDateOpenChange(event) {}
+  handleLeadStatusChange(event){}
+  handleDatePanelChange(event) { }
+  async handleCampaignChange(event) {
+    console.log("selected campaign changed to: ", event);
+    await this.getLeadMappings();
+    this.getDispositionForCampaign("asd");
+  }
+
+  leadId: string;
   subscribeToQueryParamChange(){
     const { id } = this.activatedRouter.snapshot.queryParams;
     if (!id) { return; }
 
-    this.ticketId = id;
-    this.leadsService.getLeadById(id).subscribe((ticket: any) => {
-      this.patchTicketToLead(ticket);
+    this.leadId = id;
+    this.leadsService.getLeadById(id).subscribe((lead: ILead) => {
+      this.selectedLead = lead;
+      this.getDispositionForCampaign(this.selectedLead.companyName);
     }, error => {
-      this.msg.error('Failed to fetch data for ticket id ', id);
+      this.msgService.error('Failed to fetch data for ticket id ', id);
     });
   }
 
-
-  patchTicketToLead(ticket) {
-    console.log('patch called');
-    this.validateForm.patchValue(ticket);
-  }
-
-  initForm() {
-    this.validateForm = this.fb.group({
-      _id: [null],
-      externalId: [null, [Validators.required]],
-      address: [null],
-      amount: [null],
-      companyName: [null],
-      createdAt: [null],
-      customerEmail: [null],
-      email: [null],
-      firstName: [null],
-      followUp: [null],
-      lastName: [null],
-      leadStatus: [null],
-      phoneNumber: [null],
-      phoneNumberPrefix: ['+91'],
-      product: [null],
-      remarks: [null],
-      source: [null],
-      updatedAt: [null],
-      disposition: [null]
-    });
-  }
-
-
-  subscribeToLeadIdChange() {
-    this.validateForm.get('externalId').valueChanges
-      .pipe(debounceTime(300), distinctUntilChanged())
-      .subscribe(change => {
-        console.log(change);
-        this.leadsService.suggestLead(change).subscribe((suggestedLeads: any[]) => {
-          this.leadOptions = suggestedLeads.map(lead => lead.externalId);
-        });
-      });
-  }
-
-  fetchSelectedLead(event) {
-    this.leadsService.getLeadById(event.nzValue).subscribe((lead: any) => {
-      this.validateForm.patchValue({
-        _id: lead._id,
-        externalId: lead.externalId,
-        address: lead.address,
-        amount: lead.amount,
-        companyName: lead.companyName,
-        createdAt: lead.createdAt,
-        customerEmail: lead.customerEmail,
-        email: lead.email,
-        firstName: lead.firstName,
-        followUp: lead.followUp,
-        lastName: lead.lastName,
-        leadStatus: lead.leadStatus,
-        phoneNumber: lead.phoneNumber,
-        phoneNumberPrefix: lead.phoneNumberPrefix,
-        product: lead.product,
-        remarks: lead.remarks,
-        source: lead.source,
-        updatedAt: lead.updatedAt,
-      });
-    });
-  }
-
-
-  submitForm(): void {
-    for (const i in this.validateForm.controls) {
-      this.validateForm.controls[i].markAsDirty();
-      this.validateForm.controls[i].updateValueAndValidity();
-    }
-
-    if (!this.ticketId) {
-      this.createTicket();
-    }else{
-      this.updateTicket();
-    }
-  }
-
-
-  createTicket() {
-    this.leadsService.addLead(this.validateForm.value).subscribe(data => {
-      this.msg.success('Successfully added ticket');
-    }, error => {
-      this.msg.error('Something went wrong while adding ticket');
-    });
-  }
-
-
-
-  updateTicket() {
-    this.leadsService.updateLead(this.ticketId, this.validateForm.value).subscribe(data => {
-      this.msg.success(`Successfully updated ticket with id ${this.ticketId}`);
-    }, error => {
-      this.msg.error(`Failed to update ticket with id ${this.ticketId}`);
-    });
-  }
-
-  updateConfirmValidator(): void {
-    /** wait for refresh value */
-    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
-  }
-
-
-  getCaptcha(e: MouseEvent): void {
-    e.preventDefault();
-  }
-
-
-  typeDict: { [key: string]: { label: string, value: string, type: string, checked: boolean } };
-  dataLoaded: boolean = false;
-  showCols: any[] = [];
-  mapInternalToReadableFields() {
-    this.leadsService.getAllLeadColumns().subscribe((mSchema: { paths: ILeadColumn[] }) => {
-      mSchema.paths.forEach((path: ILeadColumn) => {
-        this.showCols.push({
-          label: path.readableField,
-          value: path.internalField,
-          checked: path.checked,
-          type: path.type
-        });
-      });
-
-      console.log("Successfully created mappings");
-      this.typeDict = Object.assign({}, ...this.showCols.map((x) => ({ [x.value]: x })));
-    }, error => {
-      console.log("Unable to create mappings")
-    })
-  }
 }
