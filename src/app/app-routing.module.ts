5c7dc60a53febda09e05d934afe0c0cf5aa6c5e0
shanurrahman <shanur.cse.nitap@gmail.com>
2020-10-07 18:33:54 +0530

    added simple pagination for ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from 'src/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginComponent,
     pathMatch: 'full'
   },
@@ -14,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 ];
