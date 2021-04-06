0c75d90131a6836732c12b12f6f1bdb5ddf520f8
shanurrahman <shanur.cse.nitap@gmail.com>
2021-04-07 00:28:10 +0530

    fixed lead solo card header issues, removed ant design header

--- src/app/home/modantd/modantd.module.ts
+++ src/app/home/modantd/modantd.module.ts
@@ -9,33 +9,8 @@ import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-// import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
-// import { NzPopoverModule } from 'ng-zorro-antd/popover';
-// import { NzProgressModule } from 'ng-zorro-antd/progress';
-// import { NzRadioModule } from 'ng-zorro-antd/radio';
-// import { NzRateModule } from 'ng-zorro-antd/rate';
-// import { NzResultModule } from 'ng-zorro-antd/result';
-// import { NzSelectModule } from 'ng-zorro-antd/select';
-// import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
-// import { NzSliderModule } from 'ng-zorro-antd/slider';
-// import { NzSpinModule } from 'ng-zorro-antd/spin';
-// import { NzStatisticModule } from 'ng-zorro-antd/statistic';
-// import { NzStepsModule } from 'ng-zorro-antd/steps';
-// import { NzSwitchModule } from 'ng-zorro-antd/switch';
-// import { NzTableModule } from 'ng-zorro-antd/table';
-// import { NzTabsModule } from 'ng-zorro-antd/tabs';
-// import { NzTagModule } from 'ng-zorro-antd/tag';
-// import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
-// import { NzTimelineModule } from 'ng-zorro-antd/timeline';
-// import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-// import { NzTransferModule } from 'ng-zorro-antd/transfer';
-// import { NzTreeModule } from 'ng-zorro-antd/tree';
-// import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
-// import { NzTypographyModule } from 'ng-zorro-antd/typography';
-// import { NzUploadModule } from 'ng-zorro-antd/upload';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzContextMenuServiceModule, NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
@@ -60,7 +35,6 @@ import { NzTimelineModule } from 'ng-zorro-antd/timeline';
     NzMessageModule,
     NzModalModule,
     NzEmptyModule,
-    NzPageHeaderModule,
     NzPaginationModule,
     NzUploadModule,
     NzTimelineModule
