38147ac939231e6dd7d20c9bfa55b37e7ea447a4
Shanur Rahman <shanur.cse.nitap@gmail.com>
2022-12-24 16:14:13 +0530

    removing bin files

--- src/app/home/leads.service.ts
+++ src/app/home/leads.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import * as saveAs from "file-saver";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { ICampaign } from "../campaign/campaign.interface";
